c315b4d8690e67274de3d5b54461a64bc6f4c116
YTRANSBLOX <suleebulebule@gmail.com>
2026-04-22 17:44:05 +0700

    Remove dynamicParams export from page.tsx
    Removed dynamicParams export from page.tsx.

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,8 +2,6 @@ export async function generateStaticParams() {
   return [];
 }
 
-export const dynamicParams = true;
-
 import ScriptDetail from './ScriptDetail';
 
 export default function Page() {
